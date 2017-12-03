016366ec80f4ca1f935ce5b1a06b3453cdb64696
Nourhzm <nourhzm@gmail.com>
2017-12-03 09:57:07 +0100

    à télécharger

---
+++ app/src/main/java/com/example/nourelhoudazribi/aaychfanni/demarrer_2.java
@@ -0,0 +1,13 @@
+package com.example.nourelhoudazribi.aaychfanni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class demarrer_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demarrer_2);
+    }
+}
